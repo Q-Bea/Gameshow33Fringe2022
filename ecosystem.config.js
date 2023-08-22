a4ec2c83ac5d8da137e5588f92e81dff20396b44
Bea <1123bmac@gmail.com>
2023-08-22 16:07:07 -0700

    remove cluster mode from pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,6 @@ module.exports = {
         {
             name: "Gameshow33Fringe2022",
             port: "10001",
-            exec_mode: "cluster",
-            instances: "max",
             script: "npm",
             args: "start"
         }
